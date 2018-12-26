4a82ef83dc6eebdefb5f5b6ff7af7f69287f1b5c
Kevin Rosales <kevinrosales@Kevins-MacBook-Air.local>
2018-12-26 17:42:36 -0500

    got a favicon up and running

--- pro_portfolio/src/components/Profile.js
+++ pro_portfolio/src/components/Profile.js
@@ -7,6 +7,8 @@ import ProjectsView from "./ProjectsView";
 const StylePage = styled.div`
   font-family: sans-serif;
   text-align: center;
+  background: linear-gradient(to bottom, rgba(225,240,255,1) 0%,rgba(133,190,255,1) 100%); 
+  background-size: cover;
 `;
 
 export default class Profile extends Component {
@@ -14,15 +16,19 @@ export default class Profile extends Component {
     return (
       <div>
         <StylePage>
+          
           <Parallax>
-            <h1>Welcome to my portfolio</h1>
+            <h1>Welcome to my portfolio</h1>           
           </Parallax>
+
           <Parallax>
             <AboutMe />
           </Parallax>
+
           <Parallax>
             <ProjectsView />
           </Parallax>
+
         </StylePage>
       </div>
     );
