a36950d368c9b5808b02fdc3e4816a509e9a51b4
Kevin Rosales <kevinrosales@Kevins-MacBook-Air.local>
2018-12-26 17:21:51 -0500

    changed my page to possibly a single page website

--- pro_portfolio/src/App.js
+++ pro_portfolio/src/App.js
@@ -3,8 +3,6 @@ import { Parallax, Background } from "react-parallax";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Profile from "./components/Profile";
 import NavBar from "./components/NavBar";
-import AboutMe from "./components/AboutMe";
-import ProjectsView from "./components/ProjectsView";
 
 class App extends Component {
   render() {
@@ -15,8 +13,6 @@ class App extends Component {
           <div>
             <Switch>
               <Route exact path="/" component={Profile} />
-              <Route exact path='/about' component={AboutMe}/>
-              <Route exact path='/projects' component={ProjectsView}/>
             </Switch>
           </div>
         </Router>
--- pro_portfolio/src/components/Profile.js
+++ pro_portfolio/src/components/Profile.js
@@ -1,18 +1,30 @@
-import React, { Component } from 'react'
-import styled from 'styled-components'
+import React, { Component } from "react";
+import styled from "styled-components";
 import { Parallax, Background } from "react-parallax";
+import AboutMe from "./AboutMe";
+import ProjectsView from "./ProjectsView";
 
-
-
-
-
+const StylePage = styled.div`
+  font-family: sans-serif;
+  text-align: center;
+`;
 
 export default class Profile extends Component {
   render() {
     return (
       <div>
-        <h1>Welcome to my portfolio</h1>
+        <StylePage>
+          <Parallax>
+            <h1>Welcome to my portfolio</h1>
+          </Parallax>
+          <Parallax>
+            <AboutMe />
+          </Parallax>
+          <Parallax>
+            <ProjectsView />
+          </Parallax>
+        </StylePage>
       </div>
-    )
+    );
   }
 }
