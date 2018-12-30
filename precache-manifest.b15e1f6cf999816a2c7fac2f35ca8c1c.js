4fafa457f1e4d540364f8a5e210b70d3d0ada5b3
Kevin Rosales <kevinrosales@Kevins-MacBook-Air.local>
2018-12-30 16:43:27 -0500

    Updates

---
+++ precache-manifest.b15e1f6cf999816a2c7fac2f35ca8c1c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a056ab25df04990ee540",
+    "url": "/static/js/main.a056ab25.chunk.js"
+  },
+  {
+    "revision": "81970dfb063f820e585b",
+    "url": "/static/js/1.81970dfb.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f0b645fd4d61c9c78900d2cb856bcb32",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pro_portfolio/precache-manifest.6be3ae8be26aad6eabdec3da8c0ecded.js"
+  "/precache-manifest.b15e1f6cf999816a2c7fac2f35ca8c1c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/pro_portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
