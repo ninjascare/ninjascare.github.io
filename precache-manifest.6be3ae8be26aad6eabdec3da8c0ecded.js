9b1cf60761bed8e5ef80b8844832fd0363366073
Kevin Rosales <kevinrosales@Kevins-MacBook-Air.local>
2018-12-30 16:42:06 -0500

    Updates

---
+++ precache-manifest.6be3ae8be26aad6eabdec3da8c0ecded.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a056ab25df04990ee540",
+    "url": "/pro_portfolio/static/js/main.a056ab25.chunk.js"
+  },
+  {
+    "revision": "d16e29a9bee558a928cd",
+    "url": "/pro_portfolio/static/js/1.d16e29a9.chunk.js"
+  },
+  {
+    "revision": "f3bd05bd67084a60a3e1",
+    "url": "/pro_portfolio/static/js/runtime~main.f3bd05bd.js"
+  },
+  {
+    "revision": "48603ff2df85640e7ee108b95031e337",
+    "url": "/pro_portfolio/index.html"
+  }
+];
