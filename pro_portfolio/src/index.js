f18f26b5b9d9db53fb4c33f668aafc50ca9166a8
Kevin Rosales <kevinrosales@Kevins-MacBook-Air.local>
2018-12-31 16:01:43 -0500

    testing some things for domain to show my work

--- pro_portfolio/src/index.js
+++ pro_portfolio/src/index.js
@@ -1,12 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-ReactDOM.render(
-  <Router basename={process.env.PUBLIC_URL}>
-    {" "}
-    <App />{" "}
-  </Router>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById('root'));
